d0de43558431d5a5140869d85079a1eca47abe62
Phuc Lam <phuclamtran2810@gmail.com>
2025-02-12 16:05:55 +0700

    fix: title

--- app/root.tsx
+++ app/root.tsx
@@ -77,7 +77,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Gia Phả 4.0</title>
         <Meta />
         <Links />
       </head>
